b4da6568f41dc051198242fc70bcf3e5fd7584a8
JingleDjango95 <spencerweber019@gmail.com>
2019-10-23 16:47:57 -0600

    finalized exercise solution

--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -1,2 +1,10 @@
 public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer && (temperature > 24 && temperature < 46)) {
+            return true;
+        } else if (!summer && (temperature > 24 && temperature < 36)) {
+            return true;
+        }
+        return false;
+    }
 }
